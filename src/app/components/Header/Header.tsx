742194631ba711098d3cfd89bafda7ae7840231f
Rovshan Badirkhanov <rovsanbadirxanov@gmail.com>
2026-08-18 22:50:23 +0300

    Try to fix header link

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -12,9 +12,9 @@ const Header = () => {
 		<header className={ styles.header }>
 
 			<div className={ styles.logo }>
-				<Link to={ '/' }>
+				<NavigationLink to={ '' }>
 					<img src={ `${ ASSETS_PATH }/images/icons/logo.png` } alt="logo" />
-				</Link>
+				</NavigationLink>
 			</div>
 
 			<div className={ styles.menu }>
